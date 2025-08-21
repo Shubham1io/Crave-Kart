60fe42c61fde747059a2a9d0c4134a4b44969cbe
shubham <sk229215@gmail.com>
2025-08-21 18:10:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,137 @@
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+const cardList = document.querySelector('.card-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotal = document.querySelector('.cart-total');
+
+cartIcon.addEventListener('click',()=> cartTab.classList.add('cart-tab-active'))
+closeBtn.addEventListener('click',()=> cartTab.classList.remove('cart-tab-active'));
+
+let productList = [];
+let cartProduct = [];
+
+const updateTotals = () =>{
+    let totalPrice = 0;
+
+    document.querySelectorAll('.item').forEach(item =>{
+
+        const price = parseFloat(item.querySelector('.item-total').textContent.replace('$',''));
+        totalPrice += price;
+    });
+
+    cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+}
+
+
+const showcards = ()=>{
+    productList.forEach(product =>{
+        const orderCard = document.createElement('div');
+        orderCard.classList.add('order-card');
+
+        orderCard.innerHTML = `
+        <div class="card-image">
+            <img src="${product.image}" alt="${product.name}">
+        </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#" class="btn card-btn">Add To cart</a>
+        `;
+
+        cardList.appendChild(orderCard)
+
+        const cardBtn = orderCard.querySelector('.card-btn');
+        cardBtn.addEventListener('click',(e) =>{
+            e.preventDefault();
+            addToCart(product);
+            cartTab.classList.add('cart-tab-active');
+
+        });
+
+    });
+}
+
+const addToCart = (product) =>{
+
+    const existingProduct = cartProduct.find(item => item.id === product.id);
+    if(existingProduct){
+        alert('Item is already in your cart');
+        return;
+    }
+    else{
+        cartProduct.push(product);
+    }
+
+    let quantity = 1;
+    let price = parseFloat(product.price.replace('$',''));
+
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('item');
+
+    cartItem.innerHTML = `
+    <div class="item-image">
+        <img src="${product.image}" alt="${product.name}">
+    </div>
+        <div class="detail">
+            <h4>${product.name}</h4>
+            <h4 class="item-total">${product.price}</h4>
+        </div>
+        <div class="flex">
+            <a href="#" class="quantity-btn minus">
+                <i class="fa-solid fa-minus"></i>
+            </a>
+
+            <h4 class="quantity-value">${quantity}</h4>
+            <a href="#" class="quantity-btn plus">
+                <i class="fa-solid fa-plus"></i>
+            </a>
+        </div>
+    `;
+
+    cartList.appendChild(cartItem);
+    updateTotals();
+
+    const plusBtn = cartItem.querySelector('.plus');
+    const quantityValue = cartItem.querySelector('.quantity-value');
+    const itemTotal = cartItem.querySelector('.item-total');
+    const minusBtn = cartItem.querySelector('.minus');
+
+    plusBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        quantity++;
+        quantityValue.textContent = quantity;
+        itemTotal.textContent = `$${(price*quantity).toFixed(2)}`;
+        updateTotals();
+    })
+
+    minusBtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        if(quantity > 1){
+            quantity--;
+            quantityValue.textContent = quantity;
+            itemTotal.textContent = `$${(price*quantity).toFixed(2)}`;
+            updateTotals();
+        }
+        else{
+            cartItem.remove();
+            cartProduct = cartProduct.filter(item => item.id != product.id);
+            updateTotals();
+        }
+    })
+
+}
+
+
+const initApp = () =>{
+
+
+    fetch('productlist.json').then
+    (response => response.json()).then
+    (data =>{
+
+        productList = data;
+        showcards();        
+    })
+}
+
+initApp();
